06768e171af145c084c21e5c776055472f5a01ca
qinai8 <89003227+qinai8@users.noreply.github.com>
2021-09-06 09:14:20 +0800

    Update Http.js

--- src/router/Http.js
+++ src/router/Http.js
@@ -9,12 +9,10 @@ import router from "@/router";
 // });
 
 instance.interceptors.request.use(config => {
-    // 在发送请求之前做些什么
     // 给每个请求添加token
     config.headers['AdminToken'] = Cookies.get('token');
     return config;
   }, error => {
-    // 对请求错误做些什么
     return Promise.reject(error);
   });
 
